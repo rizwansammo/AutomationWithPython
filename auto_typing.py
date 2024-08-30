f03d916a96b435dea7246890e7817776d301305a
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-30 23:10:00 +0600

    Auto Typing

---
+++ auto_typing.py
@@ -0,0 +1,11 @@
+# used to access time related functions
+import time
+import pyautogui
+
+# pauses the execution of the program
+# for 5 sec
+time.sleep(5)
+
+# types the string passed inside the
+# function
+pyautogui.typewrite("Geeks For Geeks!")
