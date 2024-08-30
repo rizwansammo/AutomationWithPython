26d3d81826a2290040a6c3a7e743397523e3edf3
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-30 22:54:17 +0600

    Moving the cursor and clicking on specific spots:

---
+++ moving_cursor.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+
+# moves to (519,1060) in 1 sec
+pyautogui.moveTo(519, 1060, duration = 1)
+
+# simulates a click at the present
+# mouse position
+pyautogui.click()
+
+# moves to (1717,352) in 1 sec
+pyautogui.moveTo(1717, 352, duration = 1)
+
+# simulates a click at the present
+# mouse position
+pyautogui.click()
