0d85acd4db5932be74e56ef1c82225dfe2b65f95
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-08-31 12:11:40 +0600

    Proof Reading Script with Python

---
+++ proof_reader.py
@@ -0,0 +1,13 @@
+# Python Proofreading
+# pip install lmproof
+import lmproof
+
+
+def proofread(text):
+    proofread = lmproof.load("en")
+    correction = proofread.proofread(text)
+    print("Original: {}".format(text))
+    print("Correction: {}".format(correction))
+
+
+proofread("Your Text")
