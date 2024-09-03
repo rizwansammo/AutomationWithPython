efa450ce83a64a9d0e7a82055ad8e73825e95cc9
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-03 12:32:39 +0600

    Packet Sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,1 +1,8 @@
 
+import scapy.all as scapy
+
+def packet_sniffer(interface):
+    scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
+
+interface = input("Enter interface name: ")
+packet_sniffer(interface)
