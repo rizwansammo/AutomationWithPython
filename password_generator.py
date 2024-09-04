8e1fc68306fa7b1cbe4e9024e468543203aac8ba
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-05 01:48:47 +0600

    Password Generator

--- password_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(characters) for _ in range(length))
-
-length = int(input("Enter password length: "))
-print(f"Generated Password: {generate_password(length)}")
