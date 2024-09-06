8502dc66d94268fb31d9d6ce44d2146e70b46e32
Rizwan Sammo <mrizwan.sammo@gmail.com>
2024-09-06 15:12:09 +0600

    Merge remote-tracking branch 'origin/main'
    Port Scanner

--- port_scaner.py
+++ port_scaner.py
@@ -1,4 +1,15 @@
-<<<<<<< HEAD
-c
-=======
->>>>>>> origin/main
+import socket
+
+def port_scanner(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    result = sock.connect_ex((ip, port))
+    if result == 0:
+        print(f"Port {port} is open")
+    else:
+        print(f"Port {port} is closed")
+    sock.close()
+
+target_ip = input("Enter IP address: ")
+for port in range(1, 1025):
+    port_scanner(target_ip, port)
